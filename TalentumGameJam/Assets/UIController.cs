9a1a1a9fdd08b1f3623d42f7feb80e5cf1b3f185
Alvaro Falcon Morales <falconpower702@gmail.com>
2017-10-07 20:03:51 +0100

    update-ui

---
+++ TalentumGameJam/Assets/UIController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour {
+
+    public Transform player;
+    public Text computerDisplay;
+   	
+	void Update () {
+        computerDisplay.text = player.GetComponent<HackerInfo>().currentComputerID+"-"+ player.GetComponent<HackerInfo>().currentRoomNum;
+	}
+}
