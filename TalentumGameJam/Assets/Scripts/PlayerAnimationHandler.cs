d6bb94b3e38121a15b735e2227efa41223b9d1ad
DESKTOP-GO6JIUB\laura <l4depende@gmail.com>
2017-10-07 12:22:46 +0100

    -PlayerMovement -PlayerAnimationHandler

---
+++ TalentumGameJam/Assets/Scripts/PlayerAnimationHandler.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimationHandler : MonoBehaviour {
+
+
+    Animator animator;
+
+	// Use this for initialization
+	void Start () {
+        PlayerMovement.OnKeyPressed += OnKeyPressed_HandleAnimation;
+        animator = GetComponent<Animator>();
+	}
+
+    public void OnKeyPressed_HandleAnimation(PlayerMovement.MovementEnum e)
+    {
+        //TODO take the current animation name 
+        
+        if (animator.GetCurrentAnimatorStateInfo(0).IsName(e.ToString()))
+        {
+            if (e == PlayerMovement.MovementEnum.Down)
+            {
+                animator.SetInteger("movement", 1);
+            }
+            else if (e == PlayerMovement.MovementEnum.Up)
+            {
+                animator.SetInteger("movement", 2);
+            }
+            else if (e == PlayerMovement.MovementEnum.Right)
+            {
+                animator.SetInteger("movement", 3);
+
+            }
+            else if (e == PlayerMovement.MovementEnum.Left)
+            {
+                animator.SetInteger("movement", 4);
+            }
+            else if (e == PlayerMovement.MovementEnum.Idle)
+            {
+                animator.SetInteger("movement", 0);
+            }
+        }
+    }
+}
