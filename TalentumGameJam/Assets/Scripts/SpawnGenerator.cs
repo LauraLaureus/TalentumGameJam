7e0ff2ac37caabc4077c451f2df6fb77f4e79d42
DESKTOP-GO6JIUB\laura <l4depende@gmail.com>
2017-10-07 18:52:23 +0100

    spawn

---
+++ TalentumGameJam/Assets/Scripts/SpawnGenerator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnGenerator : MonoBehaviour {
+
+    public GameObject[] prefabs;
+
+    public void spawn()
+    {
+        Vector2 jitter = new Vector2(
+                UnityEngine.Random.value,
+                UnityEngine.Random.value
+            );
+
+        Vector2 nextSpawnPosition = (Vector2)this.transform.position + jitter;
+
+        int random = (int)Mathf.Round(UnityEngine.Random.Range(0,prefabs.Length));
+
+        Instantiate(prefabs[random], nextSpawnPosition, Quaternion.identity);
+    }
+
+    
+}
