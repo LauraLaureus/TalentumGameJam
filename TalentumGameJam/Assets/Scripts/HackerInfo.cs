8dd1334f98aa0de04322c3fd949ae6eab83ce0e8
DESKTOP-GO6JIUB\laura <l4depende@gmail.com>
2017-10-07 13:24:08 +0100

    -Sequence Generation -HackerInfo -Door controller

---
+++ TalentumGameJam/Assets/Scripts/HackerInfo.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HackerInfo : MonoBehaviour{
+
+
+    public int currentComputerID { get; set; }
+    public int currentRoomNum { get; set; }
+    public List<SequenceGenerator.SequenceElement> currentSequence;
+
+
+
+    public void Start()
+    {
+        currentComputerID = 0;
+        currentRoomNum = 0;
+        currentSequence = new List<SequenceGenerator.SequenceElement>();
+    }
+
+    public void GetNextSequence(int length)
+    {
+        currentSequence = SequenceGenerator.generateSequence(length);
+    }
+
+
+    public bool matchElement(SequenceGenerator.SequenceElement element)
+    {
+        if (currentSequence[0] == element)
+        {
+            this.currentSequence.RemoveAt(0);
+            return true;
+        }
+        else
+            return false;
+    }
+}
