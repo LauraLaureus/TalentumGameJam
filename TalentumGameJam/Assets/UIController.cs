6674b478bb56131fa2f37606e1329bf87358870e
Alvaro Falcon Morales <falconpower702@gmail.com>
2017-10-08 11:24:31 +0100

    added a control for lifes

--- TalentumGameJam/Assets/UIController.cs
+++ TalentumGameJam/Assets/UIController.cs
@@ -6,9 +6,15 @@ using UnityEngine.UI;
 public class UIController : MonoBehaviour {
 
     public Transform player;
-    public Text computerDisplay;
-   	
+    public Text computerDisplay,lifeDisplay;
+    private GameManager gameManager;
+
+    void Start()
+    {
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+    }
 	void Update () {
         computerDisplay.text = player.GetComponent<HackerInfo>().currentComputerID+"-"+ player.GetComponent<HackerInfo>().currentRoomNum;
+        lifeDisplay.text = "Lifes: " + gameManager.attempts;
 	}
 }
